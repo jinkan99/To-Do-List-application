1fd1973accb625bfe5c1c2619333353e37688b1e
jinkan99 <97292222+jinkan99@users.noreply.github.com>
2022-07-07 22:31:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const input = document.getElementById('task-name')
+const addBtn = document.getElementById('btn');
+const tasks = document.getElementsByClassName('tasks');
+
+addBtn.addEventListener('click',()=>{
+  if(input.value.length !== 0){
+    const template = `
+    <div class="task-list">
+      <div class="task">
+        <label>${input.value}</label>
+      </div>
+      <div class="actions">
+        <i class="fas fa-clipboard-check fa-lg" id="complete"></i>
+        <i class="fas fa-eraser fa-lg" id="remove"></i>
+      </div>
+    </div>`
+
+    const ele = document.createElement('div');
+    ele.innerHTML = template;
+    tasks[0].appendChild(ele);
+    input.value = "";
+  }else{
+    alert("Enter a task to be added");
+  }
+  
+});
+
+
+tasks[0].addEventListener('click', (e)=>{
+  if(e.target.classList.contains('fa-clipboard-check')){
+    e.target.closest('.task-list').querySelector('.task').classList.add('completed');
+  }
+});
+
+tasks[0].addEventListener('click', (e)=>{
+  if(e.target.classList.contains('fa-eraser')){
+    e.target.closest('.task-list').remove();
+  }
+});
+
